03de5acc0546d49955e6de6c4019b676bcf7bcd5
snehab162004 <bsneha.162004@gmail.com>
2024-07-20 03:43:16 -0700

    Aligned for lg screen

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,14 +4,14 @@ import heroImg from "../assets/heroImg.png";
 function Home() {
     const textStyle = {
         width: '502px',
-        height: '367px',
-        gap: '0px',
+        height: '100px',
         opacity: 1,
         fontFamily: 'Roboto',
         fontSize: '50px',
         fontWeight: 700,
         lineHeight: '88px',
         textAlign: 'center',
+        top: '200px',
     };
 
     const h4Style = {
@@ -21,7 +21,7 @@ function Home() {
         lineHeight: '65px',
         textAlign: 'center',
         position: 'relative',
-        top: '-280px',
+        top: '-30px',  
     };
 
     const spanStyle = {
@@ -34,30 +34,31 @@ function Home() {
 
     const btnStyle = {
         width: '30%',
-        left: '40%',
-        top: '-240px',
         position: 'relative',
+        top: '-30px', 
         fontFamily: 'Source Sans Pro',
         fontSize: '20px',
         fontWeight: 400,
         lineHeight: '24px',
         textAlign: 'center',
         backgroundColor: '#1A73E8',
-    }
+    };
 
     return (
-        <div className="bg-[#EEF5FF] w-3/4 items-center m-20 h-[40%] shadow-lg shadow-[#00072B36] grid grid-rows-2 grid-cols-1">
-            <div className="flex flex-col items-center">
-                <img src={heroImg} alt="hero-img" className="h-[40%] pt-0 m-0 -mt-3" />
-                <div>
-                    <h2 style={textStyle} className="pl-5 -mt-10 m-0">
+        <div className="bg-[#EEF5FF] w-3/4 items-center m-20 mx-auto mt-[40px] h-auto shadow-lg shadow-[#00072B36] grid grid-cols-1 lg:grid-cols-2">
+            <div className="flex flex-col justify-center items-center">
+                <div className="text-center">
+                    <h2 style={textStyle} className="pl-5">
                         Let’s Learn & Earn
                     </h2>
                     <h4 style={h4Style} className="pl-5">
-                        Get a chance to win <br/> up-to <span style={spanStyle}>Rs. 15,000</span> 
+                        Get a chance to win <br /> up to <span style={spanStyle}>Rs. 15,000</span> 
                     </h4>
-                    <button className="pl-[40px] border p-5 text-white rounded-md" style={btnStyle}>Refer Now</button>
+                    <button className="pl-5 border p-5 text-white rounded-md mt-5" style={btnStyle}>Refer Now</button>
                 </div>
+            </div>
+            <div className="flex justify-center items-center">
+                <img src={heroImg} alt="hero-img" className="w-[70%] pl-[20px]" />
             </div>
         </div>
     );
